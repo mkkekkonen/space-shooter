d393caeeb21a78244edaca3111c600cd99fd6e01
mkkekkonen2 <mkkekkonen@protonmail.com>
2022-10-02 00:27:06 +0300

    Initial commit after project generation

---
+++ android/src/com/mkkekkonen/spaceshooter/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mkkekkonen.spaceshooter;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mkkekkonen.spaceshooter.SpaceShooter;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceShooter(), config);
+	}
+}
